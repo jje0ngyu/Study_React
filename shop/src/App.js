e65d41bef808b326fdeadcfc2b64d9d4732c5db0
jje0ngyu <naniaerang@naver.com>
2023-03-20 13:31:54 +0900

    routes 학습

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,35 +1,48 @@
-import './App.css';
 import { useState } from 'react';
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 import data from './data.js';
+import Detail from './Detail.js';
 
 function App() {
   let [shoes] = useState(data);
 
   return (
     <div className="App">
+
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="/">홈</Nav.Link>
+            <Nav.Link href="/detail">상세</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
-      <div className="container">
-        <div className="row">
-          {
-            data.map((a,i)=>{
-              return(
-                <Shoe i={i} shoes={shoes[i]} />
-              )
-            })
-          }
-        </div>
-      </div> 
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg'></div>
+            <div className="container">
+              <div className="row">
+                {
+                  data.map((a,i)=>{
+                    return(
+                      <Shoe i={i} shoes={shoes[i]} />
+                    )
+                  })
+                }
+              </div>
+            </div> 
+          </>
+        } />
+        <Route path="/detail" element={
+          <Detail />
+        } />
+      </Routes>
+
     </div>
   );
 }
