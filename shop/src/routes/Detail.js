06875263d394d3a01a1dd00bda22bc41547fa682
jje0ngyu <naniaerang@naver.com>
2023-03-21 16:22:34 +0900

    useEffect 학습중

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,23 +1,89 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+// styled-components의 장점
+// 1 : CSS 파일을 만들지 않아도 된다.
+// 2 : 스타일이 다른 js파일로 오염되지 않는다.
+// 2-1 > 리액트는 코드 작성이 완료되면 파일을 하나로 합치는 작용을 하기 때문에
+//     > CSS파일이 하나로 합쳐져 다른 코드들도 영향을 끼친다.
+//     > 오염을 방지하려면... ' 컴포넌트.module.css ' 처럼 module 을 사용하면 된다.
+// 3 : 페이지 로딩시간 단축
+
+
+// 단점
+// 1. JS파일이 매우 복잡해진다. (* 이 컴포넌트가 styled 인지 아니면 일반 컴포넌트인지 구분이 어렵다.)
+// 2. 중복스타일은 컴포넌트간 import할텐데 CSS와 다를 바가 없다.
+// 3. 협업 시 CSS담당의 숙련도 이슈가 있다.
+
+
+
+// Lifecycle
+// 페이지에 장착된다 (mount)
+// 업데이트 된다 (update)
+// 제거된다 (unmount)
+
+/* 아래는 옛날 방식으로 작성된 코드
+
+class Detatil2 extends React.Component {
+  componentDidMount(){
+
+  }
+  componentDidUpdate(){
+
+  }
+  componentWillUnmount(){
+
+  }
+}
+*/
 
 function Detail(props){
-  let param = useParams().id;
-  let id = props.shoes[param].id;
+  // useEffect 안에 이는 코드는 html 렌더링 후에 동작한다는 점이 특별하다.
+  // > html을 먼저 보여주고 오래 걸리는 코드를 useEffect에 넣는 것도 방법이다!
+  // > useEffect 안에 적는 코드 : 어려운 연산, 서버에서 데이터 가져오는 작업, 타이머
+  useEffect(()=>{
+    // mount, update 시 코드 실행해줌
+    console.log('안녕');
+  })
 
-  console.log(props.shoes.filter(shoe => shoe.id == param));
+  const [style, setStyle] = useState({display: 'block'})
+  setTimeout(()=>{ setStyle({display:'none'}) }, 3000)
+
+  let [count, setCount] = useState(0);
+
+  let {id} = useParams();
+  //let id = props.shoes[param].id;
+  let findItem = props.shoes.find((x)=> x.id == id);
+  //console.log(props.shoes.filter(shoe => shoe.id == param));
   
+  let YellowBtn = styled.button`
+    background : ${ props => props.bg };
+    color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
+    padding : 10px;
+  `
 
+  let Box = styled.div`
+    background : grey;
+    padding : 20px;
+  `
   return(
     <div className="container">
+      <div className="aler alert-warning" style={style}>2초 이내 구매 시 할인</div>
+      <button onClick={()=>{ setCount(count+1) }}>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p>
+          <h4 className="pt-5">{findItem.title}</h4>
+          <p>{findItem.content}</p>
+          <p>{findItem.price}원</p>
           <button className="btn btn-danger">주문하기</button> 
+          <Box>
+          <YellowBtn bg="blue">버튼</YellowBtn>
+          </Box>
         </div>
       </div>
     </div> 
